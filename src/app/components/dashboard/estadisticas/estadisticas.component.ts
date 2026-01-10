05fdb296f8d347de608ed9ce8fef7a9198119797
Angelpegon <tecangelesteban@gmail.com>
2026-01-10 00:14:42 -0500

    Mejora de componente estadistica

--- src/app/components/dashboard/estadisticas/estadisticas.component.ts
+++ src/app/components/dashboard/estadisticas/estadisticas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-estadisticas',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticasComponent implements OnInit {
 
-  constructor() { }
+  isMobile = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+      });
+
+  }
   ngOnInit(): void {
+    this.crearGraficaVentas();
+    this.crearGraficaPlatos();
+  }
+  crearGraficaVentas() {
+    new Chart('ventasChart', {
+      type: 'line',
+      data: {
+        labels: ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
+        datasets: [{
+          label: 'Ventas ($)',
+          data: [120, 190, 300, 250, 420, 380, 500],
+          fill: true,
+          tension: 0.4
+        }]
+      }
+    });
   }
 
+  crearGraficaPlatos() {
+    new Chart('platosChart', {
+      type: 'doughnut',
+      data: {
+        labels: ['Hamburguesa', 'Pizza', 'Pollo', 'Jugos'],
+        datasets: [{
+          data: [35, 25, 20, 20]
+        }]
+      }
+    });
+  }
 }
